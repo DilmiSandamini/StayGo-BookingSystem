62ff75aba3a588c3eca24c3492cb28437c937664
DilmiSandamini <jkdilmisandamini@gmail.com>
2025-08-21 21:07:08 +0530

    Initialize process completed

---
+++ StayGo-BackEnd/src/main/java/lk/ijse/gdse72/staygobackend/StayGoBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse72.staygobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayGoBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayGoBackEndApplication.class, args);
+    }
+
+}
